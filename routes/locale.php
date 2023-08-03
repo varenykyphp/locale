c48773591bfbf4741d4ba53f2190582377780472
artzoo <armin.tzoulakis@gmail.com>
2023-08-03 13:31:29 +0200

    a

--- routes/locale.php
+++ routes/locale.php
@@ -4,7 +4,7 @@
 use App\Http\Kernel;
 use Illuminate\Support\Facades\Route;
 
-use Varenyky\Http\Controllers\languageController;
+use VarenykyLocale\Http\Controllers\languageController;
 
 Route::prefix(config('varenyky.path'))->name('admin.')->middleware(resolve(Kernel::class)->getMiddlewareGroups()['web'])->group(function () {
 route::resource('/languages',languageController::class);

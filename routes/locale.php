c59c8194c2ddc79727a4a6b36068e0f7babefefb
Deniz Tezcan <10155092+deniztezcan@users.noreply.github.com>
2023-08-04 10:08:44 +0200

    Update locale.php

--- routes/locale.php
+++ routes/locale.php
@@ -7,6 +7,8 @@ use VarenykyLocale\Http\Controllers\CountryController;
 use VarenykyLocale\Http\Controllers\LanguageController;
 
 Route::prefix(config('varenyky.path'))->name('admin.')->middleware(resolve(Kernel::class)->getMiddlewareGroups()['web'])->group(function () {
-    Route::resource('/languages', LanguageController::class);
-    Route::resource('/countries', CountryController::class);
+    Route::group(['middleware' => [Authenticate::class]], function () {
+        Route::resource('/languages', LanguageController::class);
+        Route::resource('/countries', CountryController::class);
+    });
 });
